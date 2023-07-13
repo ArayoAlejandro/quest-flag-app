9c8ba2e6b89972c168694659f0044d2320786526
Alejandro <alejandroarayo426@gmail.com>
2023-07-13 21:35:29 +0200

    Add error endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,24 @@
-import express from 'express'
+import express, { type Request, type Response } from 'express'
 import { connectToDatabase } from './mongo'
 
 import scoreRouter from './controllers/Score'
 
 import cors from 'cors'
+
 connectToDatabase()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/ping', (_req, res) => {
+app.get('/api/ping', (_req: Request, res: Response) => {
   res.json({ hola: 'hola' })
 })
 
 app.use('/api/score', scoreRouter)
 
-app.use((_req, res) => {
-  res.status(404).end()
+app.use((_req: Request, res: Response) => {
+  res.json({ error: 'error endpoint' }).status(404).end()
 })
 
 app.listen(3000, () => {
