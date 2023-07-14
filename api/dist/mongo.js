b5065a59ac41174f530b365ded22737db88c247a
Alejandro <alejandroarayo426@gmail.com>
2023-07-14 13:24:46 +0200

    Gitignore add dist folders

--- api/dist/mongo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectToDatabase = void 0;
-const mongoose_1 = require("mongoose");
-function connectToDatabase() {
-    (0, mongoose_1.connect)('mongodb+srv://flag:lFOUWlbhxv00Ub2M@cluster0.v79maj7.mongodb.net/app?retryWrites=true&w=majority');
-}
-exports.connectToDatabase = connectToDatabase;
-//# sourceMappingURL=mongo.js.map
--- api/src/index.ts
+++ api/src/index.ts
@@ -21,6 +21,6 @@ app.use((_req: Request, res: Response) => {
   res.json({ error: 'error endpoint' }).status(404).end()
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ?? 3000, () => {
   console.log('Server is listening ')
 })
