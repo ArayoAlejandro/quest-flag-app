caa0dbf5901ca26752996fdbbda71835f5c4a789
Alejandro <alejandroarayo426@gmail.com>
2023-07-16 21:16:29 +0200

    Add menu styles hover

--- frontend/src/components/Pages/ScorePage.tsx
+++ frontend/src/components/Pages/ScorePage.tsx
@@ -1,12 +1,19 @@
 
+import { useState } from 'react'
 import { useMenuScore } from '../../hooks/useMenuScore'
 import { Region } from '../../services/score'
 import { type RegionType } from '../../types/RegionsType'
 
 export const ScorePage = (): JSX.Element => {
   const { changeScoreMenu, scoreList } = useMenuScore()
-
+  const [activateMenu, setActivateMenu] = useState(0)
   const emote = ['🥇', '🥈', '🥉']
+
+  const handleClick = ({ key, regionParse }: { key: number, regionParse: RegionType }) => {
+    changeScoreMenu(regionParse)
+    setActivateMenu(key)
+  }
+
   return (
     <section className='score__game'>
       <ul className='score__game__menu__list'>
@@ -14,8 +21,9 @@ export const ScorePage = (): JSX.Element => {
           Object.keys(Region).map((region, key) => {
             const regionParse = region as RegionType
             return (
-              <li className='score__game__menu__item' key={key}>
-                <a onClick={() => { changeScoreMenu(regionParse) }}>
+
+              <li className={`score__game__menu__item ${activateMenu === key ? 'score__game__menu__item-activate' : ''}`} key={key}>
+                <a onClick={() => { handleClick({ key, regionParse }) }}>
                   {region.toUpperCase()}
                 </a>
               </li>
@@ -23,30 +31,38 @@ export const ScorePage = (): JSX.Element => {
           })
         }
       </ul>
-      <ul className='score__game__list'>
-        <li className='score__game__list__header' >
-          <p>
-            Clasificación
-          </p>
-        </li>
-        {
-          scoreList.map((score, index) =>
-            <li className='score__game__item' key={score.id}>
+      {
+        scoreList.length === 0
+          ? <div>
+            <p>
+              No se han encontrado puntuaciones para este juego
+            </p>
+          </div>
+          : <ul className='score__game__list'>
+            <li className='score__game__list__header' >
               <p>
-                {
-                  index > 2
-                    ? `${index + 1}. `
-                    : emote[index]
-                }
-                {score.name}
-              </p>
-              <p>
-                {score.score} pts
+                Clasificación
               </p>
             </li>
-          )
-        }
-      </ul>
+            {
+              scoreList.map((score, index) =>
+                <li className='score__game__item' key={score.id}>
+                  <p>
+                    {
+                      index > 2
+                        ? `${index + 1}. `
+                        : emote[index]
+                    }
+                    {score.name}
+                  </p>
+                  <p>
+                    {score.score} pts
+                  </p>
+                </li>
+              )
+            }
+          </ul>
+      }
     </section>
   )
 }
