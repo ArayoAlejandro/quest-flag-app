2de2898eac59e1863ad27780e8be0b0745c1e7f1
Alejandro <alejandroarayo426@gmail.com>
2023-12-26 14:09:40 +0100

    Change env mongoDB

--- api/src/mongo.ts
+++ api/src/mongo.ts
@@ -2,5 +2,5 @@ import { connect } from 'mongoose'
 import 'dotenv/config'
 
 export function connectToDatabase () {
-  connect(process.env.MONGODB as string)
+  connect(process.env.MONGODB_URI as string)
 }
